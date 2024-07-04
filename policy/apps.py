7ec29fc09eda5669b3d8293d6b359f712e6ab4d2
김영한 <kim-younghan@gim-yeonghan-ui-MacBookPro.local>
2024-07-04 14:09:46 +0900

    second commit

--- policy/apps.py
+++ policy/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class IdbsVtunerConfig(AppConfig):
+class IdbsPolicyConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'idbs_vtuner'
+    name = 'policy'
--- vtuner/apps.py
+++ vtuner/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class IdbsPolicyConfig(AppConfig):
+class VtunerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'idbs_policy'
+    name = 'vtuner'
